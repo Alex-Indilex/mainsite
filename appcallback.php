a82877ad1ee3506fd2c43b65bf9e3d9b1eb2359a
Alexey <alexindilex@yandex.ru>
2020-01-09 07:17:41 +0300

    add first

---
+++ appcallback.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Создание формы обратной связи</title>
+<meta http-equiv="Refresh" content="4; URL=http://indilex.ru"> 
+</head>
+<body>
+
+<?php 
+
+$sendto   = "bukinist@inbox.ru"; // почта, на которую будет приходить письмо
+$username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
+$usertel = $_POST['telephone']; // сохраняем в переменную данные полученные из поля c телефонным номером
+$usermail = 'zvonok@indilex.ru';
+
+	
+	
+// Формирование заголовка письма
+$subject  = "Обратный звонок с сайта Indilex Creation";
+$headers  = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+
+// Формирование тела письма
+$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Обратный звонок с сайта Indilex Creation</h2>\r\n";
+$msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
+$msg .= "<p><strong>Телефон:</strong> ".$usertel."</p>\r\n";
+$msg .= "</body></html>";
+
+// отправка сообщения
+if(@mail($sendto, $subject, $msg, $headers)) {
+	echo "<center><img class='img-fluid' src='img/spasibo.png'></center>";
+} else {
+	echo "<center><img class='img-fluid' src='img/ne-otpravleno.png'></center>";
+}
+
+?>
+
+</body>
+</html>
---
+++ application.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Создание формы обратной связи</title>
+<meta http-equiv="Refresh" content="4; URL=http://indilex.ru"> 
+</head>
+<body>
+
+<?php 
+
+$sendto   = "fedyanin1981@mail.ru"; // почта, на которую будет приходить письмо
+$username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
+$usertel = $_POST['telephone']; // сохраняем в переменную данные полученные из поля c телефонным номером
+$usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
+$usermessage = $_POST['message']; // сохраняем в переменную данные полученные из поля cообщения
+	
+	
+// Формирование заголовка письма
+$subject  = "Сообщение с сайта Indilex Creation";
+$headers  = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+
+// Формирование тела письма
+$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Сообщение с сайта Indilex Creation</h2>\r\n";
+$msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
+$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+$msg .= "<p><strong>Телефон:</strong> ".$usertel."</p>\r\n";
+$msg .= "<p><strong>Сообщение:</strong> ".$usermessage."</p>\r\n";
+$msg .= "</body></html>";
+
+// отправка сообщения
+if(@mail($sendto, $subject, $msg, $headers)) {
+	echo "<center><img class='img-fluid' src='img/spasibo.png'></center>";
+} else {
+	echo "<center><img class='img-fluid' src='img/ne-otpravleno.png'></center>";
+}
+
+?>
+
+</body>
+</html>
